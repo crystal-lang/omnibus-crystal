7c66e619f40aaf262de10593069a9382c4bae96b
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-08-27 19:17:49 -0300

    Use official gc source file from www.hboehm.info

--- config/software/bdw-gc.rb
+++ config/software/bdw-gc.rb
@@ -1,18 +1,16 @@
 name "bdw-gc"
 default_version "7.4.2"
 
-source :url => "https://github.com/ivmai/bdwgc/archive/gc7_4_2.tar.gz",
-       :md5 => "ef64e9927f4670daebc4b9399278fb72"
+source :url => "http://www.hboehm.info/gc/gc_source/gc-#{version}.tar.gz",
+       :md5 => "12c05fd2811d989341d8c6d81f66af87"
 
 dependency "libatomic_ops"
 
-relative_path 'bdwgc-gc7_4_2'
+relative_path "gc-#{version}"
 
 env = with_standard_compiler_flags
 
 build do
-  command "autoreconf -vif"
-  command "automake --add-missing"
   command "./configure" \
           " --disable-debug" \
           " --disable-dependency-tracking" \
