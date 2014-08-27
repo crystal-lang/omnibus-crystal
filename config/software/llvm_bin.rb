59233bc54f1711399c724a289423819af7ce86d5
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-08-27 19:43:43 -0300

    Added llvm configuration for Ubuntu 12.04 64 bits

--- config/software/llvm_bin.rb
+++ config/software/llvm_bin.rb
@@ -1,10 +1,19 @@
 name "llvm_bin"
 default_version "3.3"
 
-source :url => "http://llvm.org/releases/3.3/clang+llvm-3.3-x86_64-apple-darwin12.tar.gz",
-       :md5 => "3c9984ca05f68ca5dc7e06dad92a96ab"
+if mac_os_x?
+  source url: "http://llvm.org/releases/3.3/clang+llvm-3.3-x86_64-apple-darwin12.tar.gz",
+         md5: "3c9984ca05f68ca5dc7e06dad92a96ab"
 
-relative_path 'clang+llvm-3.3-x86_64-apple-darwin12'
+  relative_path 'clang+llvm-3.3-x86_64-apple-darwin12'
+elsif ubuntu? && _64_bit?
+  source url: "http://llvm.org/releases/3.3/clang+llvm-3.3-amd64-Ubuntu-12.04.2.tar.gz",
+         md5: "1059f3232b2eef6f0bee7000a99694a7"
+
+  relative_path 'clang+llvm-3.3-amd64-Ubuntu-12.04.2'
+end
+
+
 
 build do
   copy "#{project_dir}/lib/libLLVM*.a", "#{install_dir}/embedded/lib/"
