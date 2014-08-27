184722c59ec50fdcb08eacfb85968a31092c3932
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-08-26 21:26:37 -0300

    Fix error to copy llvm library files

--- config/software/llvm_bin.rb
+++ config/software/llvm_bin.rb
@@ -7,9 +7,7 @@ source :url => "http://llvm.org/releases/3.3/clang+llvm-3.3-x86_64-apple-darwin1
 relative_path 'clang+llvm-3.3-x86_64-apple-darwin12'
 
 build do
-  `#{project_dir}/bin/llvm-config --libnames`.split.each do |lib|
-    copy "#{project_dir}/lib/#{lib}", "#{install_dir}/embedded/lib/#{lib}"
-  end
+  copy "#{project_dir}/lib/libLLVM*.a", "#{install_dir}/embedded/lib/"
 
   %w(clang llvm-config opt llc).each do |bin|
     copy "#{project_dir}/bin/#{bin}", "#{install_dir}/embedded/bin/#{bin}"
