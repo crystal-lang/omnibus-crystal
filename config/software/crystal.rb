b5646cc2225f614808df10112230b6a1e4739b97
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-10-30 01:57:46 -0300

    Crystal 0.9.1 and shards 0.5.3

--- config/software/crystal.rb
+++ config/software/crystal.rb
@@ -1,4 +1,4 @@
-CRYSTAL_VERSION = "0.9.0"
+CRYSTAL_VERSION = "0.9.1"
 
 name "crystal"
 default_version CRYSTAL_VERSION
--- config/software/shards.rb
+++ config/software/shards.rb
@@ -1,17 +1,16 @@
 name "shards"
-default_version "0.5.1"
+default_version "0.5.3"
 
 dependency "crystal"
 dependency "libyaml"
 
 source url: "https://github.com/ysbaddaden/shards/archive/v#{version}.tar.gz",
-       md5: "e61930c5eefa21a9ed7a5a5f2302d9ff"
+       md5: "6dac5b08a158ec56011a1708f44da155"
 
 relative_path "shards-#{version}"
 env = with_standard_compiler_flags(with_embedded_path)
 
 build do
-  patch source: "shards.patch"
   command "#{install_dir}/bin/crystal" \
           " build" \
           " -o #{install_dir}/embedded/bin/shards" \
