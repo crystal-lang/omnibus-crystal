550a70093f1c33cca6947810ddae48db1deebb7a
Jonne Haß <me@jhass.eu>
2016-07-11 14:36:41 +0200

    fix pcre download URL

--- config/software/pcre.rb
+++ config/software/pcre.rb
@@ -17,7 +17,7 @@
 name "pcre"
 default_version "8.31"
 
-source url: "http://iweb.dl.sourceforge.net/project/pcre/pcre/#{version}/pcre-#{version}.tar.gz",
+source url: "http://downloads.sourceforge.net/project/pcre/pcre/#{version}/pcre-#{version}.tar.gz",
        md5: "fab1bb3b91a4c35398263a5c1e0858c1"
 
 relative_path "pcre-#{version}"
