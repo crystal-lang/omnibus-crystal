57e0ebcf5a04bdd205c85ae5ba125d659e8de2ca
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-08-03 20:11:24 -0300

    Upgrade llvm build to 3.8.1

--- config/software/llvm.rb
+++ config/software/llvm.rb
@@ -1,8 +1,8 @@
 name "llvm"
-default_version "3.5.0"
+default_version "3.8.1"
 
-source :url => "http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz",
-       :md5 => "d6987305a1a0e58e128c1374cd3b8fef"
+source :url => "http://llvm.org/releases/3.8.1/llvm-3.8.1.src.tar.xz",
+       :md5 => "538467e6028bbc9259b1e6e015d25845"
 
 relative_path "llvm-#{version}.src"
 
@@ -13,18 +13,17 @@ whitelist_file "lib/LLVMHello.dylib"
 env = with_standard_compiler_flags(with_embedded_path)
 
 build do
-  patch source: 'PR20800.patch', plevel: 0
-  command "./configure" \
-          " --enable-targets=host" \
-          " --disable-terminfo" \
-          " --disable-libffi" \
-          " --disable-zlib" \
-          " --disable-docs" \
-          " --prefix=#{install_dir}" \
-          " --disable-shared" \
-          " --enable-optimized" \
-          " --disable-assertions", env: env
+  mkdir "build"
 
-  make "-j #{workers}", env: env
-  make "install"
+  command "cmake" \
+          " -DLLVM_TARGETS_TO_BUILD=host" \
+          " -DCMAKE_BUILD_TYPE=MinSizeRel" \
+          " -DLLVM_BUILD_TOOLS=OFF" \
+          " -DLLVM_ENABLE_ZLIB=OFF" \
+          " -DCMAKE_INSTALL_PREFIX=#{install_dir}" \
+          " #{project_dir}", env: env,
+                             cwd: "#{project_dir}/build"
+
+  command "cmake --build . -- -j#{workers}", env: env, cwd: "#{project_dir}/build"
+  command "cmake --build . --target install", env: env, cwd: "#{project_dir}/build"
 end
