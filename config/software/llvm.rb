64c122c851360fb90b8d2845f2f95ecf6ca81b03
Matias Garcia Isaia <mgarcia@manas.tech>
2017-07-07 15:20:05 -0300

    Build LLVM 3.8.1 from sources

--- config/software/llvm.rb
+++ config/software/llvm.rb
@@ -1,8 +1,8 @@
 name "llvm"
-default_version "3.5.0"
+default_version "3.8.1"
 
-source :url => "http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz",
-       :md5 => "d6987305a1a0e58e128c1374cd3b8fef"
+source :url => "http://llvm.org/releases/3.8.1/llvm-3.8.1.src.tar.xz",
+       :md5 => "538467e6028bbc9259b1e6e015d25845"
 
 relative_path "llvm-#{version}.src"
 
@@ -12,19 +12,24 @@ whitelist_file "lib/LLVMHello.dylib"
 
 env = with_standard_compiler_flags(with_embedded_path)
 
-build do
-  patch source: 'PR20800.patch', plevel: 0
-  command "./configure" \
-          " --enable-targets=host" \
-          " --disable-terminfo" \
-          " --disable-libffi" \
-          " --disable-zlib" \
-          " --disable-docs" \
-          " --prefix=#{install_dir}" \
-          " --disable-shared" \
-          " --enable-optimized" \
-          " --disable-assertions", env: env
+llvm_build_dir = "#{build_dir}/build-llvm"
 
-  make "-j #{workers}", env: env
-  make "install"
+build do
+  mkdir llvm_build_dir
+  command "cmake" \
+    " -DLLVM_TARGETS_TO_BUILD=X86" \
+    " -DLLVM_ENABLE_TERMINFO=OFF" \
+    " -DLLVM_ENABLE_FFI=OFF" \
+    " -DLLVM_ENABLE_ZLIB=OFF" \
+    " -DLLVM_BUILD_DOCS=OFF" \
+    " -DLLVM_INCLUDE_DOCS=OFF" \
+    " -DLLVM_BINARY_DIR=#{install_dir}" \
+    " -DBUILD_SHARED_LIBS=OFF" \
+    " -DLLVM_OPTIMIZED_TABLEGEN=ON" \
+    " -DLLVM_ENABLE_ASSERTIONS=OFF" \
+    "#{' -DPYTHON_EXECUTABLE=$(which python2.7)' if centos? }"\
+    " #{project_dir}", env: env, cwd: llvm_build_dir
+  command "cmake --build .", env: env, cwd: llvm_build_dir
+  command "cmake -DCMAKE_INSTALL_PREFIX=#{install_dir} -P cmake_install.cmake", env: env, cwd: llvm_build_dir
+  command "cmake --build . --target install", env: env, cwd: llvm_build_dir
 end
