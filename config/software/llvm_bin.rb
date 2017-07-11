524adc1b2bf4b28b39777734f1a627f014250543
Matias Garcia Isaia <mgarcia@manas.tech>
2017-07-11 20:58:26 -0300

    LLVM 3.8.1 binaries built with old distros support

--- config/software/llvm_bin.rb
+++ config/software/llvm_bin.rb
@@ -1,14 +1,14 @@
 name "llvm_bin"
-default_version "3.8.1-1"
+default_version "3.8.1-2"
 
 if linux?
   if _64_bit?
-    source_md5 = "f9ab34e22c3a954a85812e0c2c8dc57a"
+    source_md5 = "c76a79bebecde3e4bd6483e822ce8064"
   else
-    source_md5 = "9f27b397268bebddf1c37d7127702c97"
+    source_md5 = "28c340a86261f19358ddc329cbb6e3db"
   end
 elsif mac_os_x? && _64_bit?
-  source_md5 = "1957a71b619fb68bb3b5cec5a56723d1"
+  source_md5 = "a7d9881e90ad4bcb9d83cb7d5386cadd"
 end
 
 source url: "http://crystal-lang.s3.amazonaws.com/llvm/llvm-#{version}-#{ohai['os']}-#{ohai['kernel']['machine']}.tar.gz",
